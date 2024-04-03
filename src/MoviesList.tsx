149ffc022233ad9331d00660e9c7e9e8e0d63d2c
NIHAD779 <nigad779@gmail.com>
2024-04-04 00:52:01 +0530

    second commit

--- src/MoviesList.tsx
+++ src/MoviesList.tsx
@@ -1,26 +1,32 @@
 import React,{useState,useEffect} from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 
 const MoviesList = () => {
-    const [movies,setMovies] = useState([])
+    // const [movies,setMovies] = useState([])
     const [page,setpage] = useState(1)
     const [title,setTitle] = useState('')
 
-    const fetchMovies =  async()=>{
-        try{
-            const response = await axios.get('http://localhost:3000/movies',{
-                params:{page}
-            })
-            console.log(response)
-        }catch(error){
-            console.log(error)
-    }}
+    // const fetchMovies =  async()=>{
+    //     try{
+    //         const response = await axios.get('http://localhost:3000/movies',{
+    //             params:{page}
+    //         })
+    //         console.log(response)
+    //     }catch(error){
+    //         console.log(error)
+    // }}
 
     useEffect(()=>{
         // fetchMovies()
     },[page,title])
   return (
     <div>
+        <input
+        type='text'
+        placeholder='Search by title'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        />
       <table>
         <thead className="">
           <tr className="flex gap-10">
