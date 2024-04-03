3879b79b0c281d82cc91878c7c485474dca04eaf
NIHAD779 <nigad779@gmail.com>
2024-04-04 00:46:35 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MoviesList from './MoviesList';
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <MoviesList />
+    </>
+  );
+}
+
+export default App
---
+++ src/MoviesList.tsx
@@ -0,0 +1,45 @@
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
+
+const MoviesList = () => {
+    const [movies,setMovies] = useState([])
+    const [page,setpage] = useState(1)
+    const [title,setTitle] = useState('')
+
+    const fetchMovies =  async()=>{
+        try{
+            const response = await axios.get('http://localhost:3000/movies',{
+                params:{page}
+            })
+            console.log(response)
+        }catch(error){
+            console.log(error)
+    }}
+
+    useEffect(()=>{
+        // fetchMovies()
+    },[page,title])
+  return (
+    <div>
+      <table>
+        <thead className="">
+          <tr className="flex gap-10">
+            <th>Title</th>
+            <th>Genre</th>
+            <th>year</th>
+          </tr>
+        </thead>
+        <tfoot>
+            <button onClick={() => setpage(page-1)} disabled={page === 1}>
+                Prevous
+            </button>
+            <button onClick={()=> setpage(page+1)}>
+                Next
+            </button>
+        </tfoot>
+      </table>
+    </div>
+  );
+}
+
+export default MoviesList
